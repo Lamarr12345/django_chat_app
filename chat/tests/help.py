49b895894c4835cd9ce1746b84067b9cf624c8e1
Lamarr12345 <richter_malte@web.de>
2025-12-18 16:22:50 +0100

    fix:naming

---
+++ chat/tests/help.py
@@ -0,0 +1,20 @@
+# def setUp(self):
+#         self.user = models.User.objects.create_user(
+#             username = 'test',
+#             email = 'test@gmail.com',
+#             password = 'Test1234'
+#         )
+#         self.redirect_to_home_url = reverse('chat:redirect-to-home')
+#         self.home_url = reverse('chat:home')
+#         self.login_url = reverse('chat:login')
+#         self.logout_url = reverse('chat:logout')
+#         self.signup_url = reverse('chat:signup')
+#         self.user_home_url = reverse('chat:user-home')
+#         self.user_public_chats_url = reverse('chat:user-public-chats')
+#         self.user_public_chat_room_url = reverse('chat:user-public-chat-room')
+#         self.user_private_chats_url = reverse('chat:user-private-chats')
+#         self.user_private_chat_room_url = reverse('chat:user-private-chat-room')
+#         self.user_private_chat_room_join_url = reverse('chat:user-private-chat-room-join')
+#         self.user_public_chat_room_join_url = reverse('chat:user-public-chat-room-join')
+#         self.user_public_chat_room_close_url = reverse('chat:user-public-chat-room-close')
+
