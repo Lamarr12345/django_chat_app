633ea0e041d82fa77b28db0922b14558dffe2a7d
Lamarr12345 <richter_malte@web.de>
2026-02-05 11:09:12 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mrdcistudent.pythonanywhere.com']
 
 
 # Application definition
@@ -175,4 +175,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
     'http://localhost',
+    'https://mrdcistudent.pythonanywhere.com/'
 ]
